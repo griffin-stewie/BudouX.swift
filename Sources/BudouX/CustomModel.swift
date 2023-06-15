33b4753784da50fe45978457d2f0514a3002c896
griffin-stewie <panterathefamilyguy@gmail.com>
2023-06-15 21:32:20 +0900

    add an initializer variant

--- Sources/BudouX/CustomModel.swift
+++ Sources/BudouX/CustomModel.swift
@@ -12,6 +12,12 @@ public struct CustomModel: Model {
     public let supportedNaturalLanguages: Set<String>
     public let featureAndScore: [String: [String: Int]]
 
+    public init(fileURL: URL, supportedNaturalLanguages: Set<String>) throws {
+        precondition(fileURL.isFileURL)
+        let data = try Data(contentsOf: fileURL)
+        try self.init(modelJSON: data, supportedNaturalLanguages: supportedNaturalLanguages)
+    }
+
     public init(modelJSON: Data, supportedNaturalLanguages: Set<String>) throws {
         featureAndScore = try JSONDecoder().decode([String: [String: Int]].self, from: modelJSON)
         self.supportedNaturalLanguages = supportedNaturalLanguages
